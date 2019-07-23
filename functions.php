ff6d52fd6942b1f8b42a69fa42911d772d8ace6a
Ricky Angeles <ricky@improveandgrow.com>
2019-07-23 09:47:15 -0400

    again

--- functions.php
+++ functions.php
@@ -31,3 +31,16 @@ function add_child_theme_textdomain() {
     load_child_theme_textdomain( 'understrap-child', get_stylesheet_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
+
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/rickyangeles/irex-theme',
+	__FILE__,
+	'irex-theme'
+);
+
+// //Optional: If you're using a private repository, specify the access token like this:
+// $myUpdateChecker->setAuthentication('your-token-here');
+//
+// //Optional: Set the branch that contains the stable release.
+// $myUpdateChecker->setBranch('stable-branch-name');
