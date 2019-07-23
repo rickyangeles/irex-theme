78863e40af0321e5da69bb0645b41048841fc5ae
Ricky Angeles <ricky@improveandgrow.com>
2019-07-23 13:47:42 -0400

    Starting with v0.0.1

--- functions.php
+++ functions.php
@@ -39,8 +39,10 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'irex-theme'
 );
 
+ $myUpdateChecker->getVcsApi()->enableReleaseAssets();
+
 // //Optional: If you're using a private repository, specify the access token like this:
 // $myUpdateChecker->setAuthentication('your-token-here');
 //
-// //Optional: Set the branch that contains the stable release.
-// $myUpdateChecker->setBranch('stable-branch-name');
+//Optional: Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('master');
